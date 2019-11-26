9a3a96124f816b06f987fe587dac3e655d6f7bfd
Scott Bassett <S.Bassett@ucas.ac.uk>
2019-11-26 17:08:21 +0000

    Almost done stage two

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -16,18 +16,27 @@ namespace BusBoard.ConsoleApp
         static void Main(string[] args)
 
         {
-            //while (true)
+            while (true)
             {
-               // Console.WriteLine("Please enter stop code");
+                Console.WriteLine("Please enter your postcode to see the next buses at the two nearest bus stops:");
                 string userInput = Console.ReadLine();
+                
 
                 ServicePointManager.Expect100Continue = true;
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
                 {
+                    var postcodeClient = new RestClient("http://api.postcodes.io");
+                    var postcodeRequest = new RestRequest($"/postcodes/{userInput}", Method.GET) { RequestFormat = DataFormat.Json };
+                    IRestResponse postcodeResponse = postcodeClient.Execute(postcodeRequest);
+                    string postcodeContent = postcodeResponse.Content;
+
+
+                    PostCodeResult postcodeObject = JsonConvert.DeserializeObject<PostCodeResult>(postcodeContent);
+
                     var tflClient = new RestClient("https://api.tfl.gov.uk");
                     var arrivalsRequest = new RestRequest("/StopPoint/490008660N/Arrivals", Method.GET) { RequestFormat = DataFormat.Json };
-                    var stopPointRequest = new RestRequest("StopPoint?stopTypes=NaptanOnstreetBusCoachStopPair&radius=200&lat=51.553935&lon=-0.144754", Method.GET) { RequestFormat = DataFormat.Json };
+                    var stopPointRequest = new RestRequest($"StopPoint?stopTypes=NaptanOnstreetBusCoachStopPair&radius=200&lat={postcodeObject.result.latitude}&lon={postcodeObject.result.longitude}", Method.GET) { RequestFormat = DataFormat.Json };
                     IRestResponse arrivalsResponse = tflClient.Execute(arrivalsRequest);
                     IRestResponse stopPointResponse = tflClient.Execute(stopPointRequest);
                     string arrivalsContent = arrivalsResponse.Content;
@@ -36,16 +45,14 @@ namespace BusBoard.ConsoleApp
                     List<Bus> busList = JsonConvert.DeserializeObject<List<Bus>>(arrivalsContent);
                     List<Bus> sortedBusList = busList.OrderBy(o => o.timeToStation).ToList();
 
-                    List<StopPoint> stopPointList = JsonConvert.DeserializeObject<List<StopPoint>>(stopPointContent);
+                    StopPointResult stopPointObject = JsonConvert.DeserializeObject<StopPointResult>(stopPointContent);
+                    //PostCode postcodeObject = JsonConvert.DeserializeObject<PostCode>(postcodeContent);
 
-                    var postcodeClient = new RestClient("http://api.postcodes.io");
-                    var postcodeRequest = new RestRequest("/postcodes/NW5 1TL", Method.GET) { RequestFormat = DataFormat.Json };
-                    IRestResponse postcodeResponse = postcodeClient.Execute(postcodeRequest);
-                    string postcodeContent = postcodeResponse.Content;
+                  //var StopPoint.StopPointResults = new StopPoint(); 
 
-                    PostCode postcodeObject = JsonConvert.DeserializeObject<PostCode>(postcodeContent);                    
-                  
-                    Console.WriteLine($"Latitude = {postcodeObject.result.latitude}");
+
+
+                    Console.WriteLine($"{stopPointObject.stopPoints}");
 
                     Console.ReadLine();
                 }
