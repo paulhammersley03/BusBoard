01032941c4f9fe7b95a6e7fb9b0c43511ecb8e56
Paul Hammersley <P.Hammersley@ucas.ac.uk>
2019-11-26 15:56:58 +0000

    successfully imported and converted post code data

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -34,48 +34,20 @@ namespace BusBoard.ConsoleApp
                     string stopPointContent = stopPointResponse.Content;
 
                     List<Bus> busList = JsonConvert.DeserializeObject<List<Bus>>(arrivalsContent);
-                    List<Bus> sortedBusList = busList.OrderBy(o => o.timeToStation).ToList();               
+                    List<Bus> sortedBusList = busList.OrderBy(o => o.timeToStation).ToList();
 
-                    //Regex UserInput = new Regex(@"List\s\b(?<firstname>)");
-                  
-                    //if (userInput == "490008660N")
-                    //{
-                    //    foreach (var bus in sortedBusList.Take(5))
-                    //    {
-                    //        Console.WriteLine($"{bus.id} + {bus.destinationName} + {bus.timeToStation}");
-                    //    }                           
-                    //}
-                    //else
-                    //{
-                    //    Console.WriteLine("Invalid Input");
-                    //}
+                    List<StopPoint> stopPointList = JsonConvert.DeserializeObject<List<StopPoint>>(stopPointContent);
 
                     var postcodeClient = new RestClient("http://api.postcodes.io");
                     var postcodeRequest = new RestRequest("/postcodes/NW5 1TL", Method.GET) { RequestFormat = DataFormat.Json };
                     IRestResponse postcodeResponse = postcodeClient.Execute(postcodeRequest);
-                    var postcodeContent = postcodeResponse.Content;
+                    string postcodeContent = postcodeResponse.Content;
 
-
-                   List<PostCode> postcodeList = JsonConvert.DeserializeObject<List<PostCode>>(postcodeContent);
-                    //ArrayList postCodeList = new ArrayList();
-                    // var postCode = JsonConvert.DeserializeObject<PostCode>(postcodeContent);
-                    //ArrayList  busList = JsonConvert.DeserializeObject<List<Bus>>(arrivalsContent);
-                    //JObject p = JObject.Parse(postcodeContent);
-
-
-
-
-                    //foreach (var postCode1 in postcodeContent)
-                    //{
-                    PostCode.longLat test1 = new PostCode.longLat();
-                    Console.WriteLine(test1);
-                    //}
+                    PostCode postcodeObject = JsonConvert.DeserializeObject<PostCode>(postcodeContent);                    
+                  
+                    Console.WriteLine($"Latitude = {postcodeObject.result.latitude}");
 
                     Console.ReadLine();
-
-                   
-
-                    //Try to ensure you're using a sensible class structure with well-named methods. Remember to commit and push your changes as you go.
                 }
             }    
         }   
---
+++ BusBoard.ConsoleApp/StopPoint.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusBoard.ConsoleApp
+{
+    
+    public class StopPoint
+    {
+        public string __invalid_name__$type { get; set; }
+        public List<double> centrePoint { get; set; }
+        public List<StopPoint> stopPoints { get; set; }
+        public int pageSize { get; set; }
+        public int total { get; set; }
+        public int page { get; set; }
+    }   
+
+    public class Crowding
+    {
+        public string __invalid_name__$type { get; set; }
+    }
+
+    public class Line
+    {
+        public string __invalid_name__$type { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string uri { get; set; }
+        public string type { get; set; }
+        public Crowding crowding { get; set; }
+        public string routeType { get; set; }
+        public string status { get; set; }
+    }
+
+    public class LineGroup
+    {
+        public string __invalid_name__$type { get; set; }
+        public string naptanIdReference { get; set; }
+        public string stationAtcoCode { get; set; }
+        public List<string> lineIdentifier { get; set; }
+    }
+
+    public class LineModeGroup
+    {
+        public string __invalid_name__$type { get; set; }
+        public string modeName { get; set; }
+        public List<string> lineIdentifier { get; set; }
+    }
+
+    public class Child
+    {
+        public string __invalid_name__$type { get; set; }
+        public string naptanId { get; set; }
+        public string indicator { get; set; }
+        public string stopLetter { get; set; }
+        public List<object> modes { get; set; }
+        public string icsCode { get; set; }
+        public string stationNaptan { get; set; }
+        public List<object> lines { get; set; }
+        public List<object> lineGroup { get; set; }
+        public List<object> lineModeGroups { get; set; }
+        public bool status { get; set; }
+        public string id { get; set; }
+        public string commonName { get; set; }
+        public string placeType { get; set; }
+        public List<object> additionalProperties { get; set; }
+        public List<object> children { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+
+    public class StopPoint
+    {
+        public string __invalid_name__$type { get; set; }
+        public string naptanId { get; set; }
+        public List<string> modes { get; set; }
+        public string icsCode { get; set; }
+        public string stopType { get; set; }
+        public string stationNaptan { get; set; }
+        public List<Line> lines { get; set; }
+        public List<LineGroup> lineGroup { get; set; }
+        public List<LineModeGroup> lineModeGroups { get; set; }
+        public bool status { get; set; }
+        public string id { get; set; }
+        public string commonName { get; set; }
+        public double distance { get; set; }
+        public string placeType { get; set; }
+        public List<object> additionalProperties { get; set; }
+        public List<Child> children { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+}
+ 
